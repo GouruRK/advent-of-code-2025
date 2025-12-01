c6b9071a9f9c0f67b914b9870000b6972f73a6b0
GouruRK <gouru.rk.gouru.rk@gmail.com>
2025-12-01 19:34:31 +0100

    add day 1 part 1

---
+++ src/fr/aoc2025/day01/AdventOfCode01P1.scala
@@ -0,0 +1,52 @@
+package fr.aoc2025.day01
+
+import java.util
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.Using
+
+object AdventOfCode01P1 {
+
+  private val EXAMPLE : String = """
+    L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82""".stripMargin.stripIndent.stripLeading.stripTrailing
+
+  private def solve(input: String): Unit = {
+    println(solve(input.split("\n").toList, 50, 0))
+  }
+
+  @tailrec
+  private def solve(lines: List[String], current: Int, acc: Int): Int = {
+    lines match {
+      case Nil => acc
+      case line::others =>
+        line match {
+          case s"L$n" =>
+            val next = (current - n.toInt) % 100
+            solve(others, next, acc + (if (next == 0) 1 else 0))
+          case s"R$n" =>
+            val next = (current + n.toInt) % 100
+            solve(others, next, acc + (if (next == 0) 1 else 0))
+        }
+    }
+  }
+
+  def main(args: Array[String]) : Unit = {
+    if (args.isEmpty) {
+      solve(EXAMPLE)
+    } else {
+      Using(Source.fromFile(args.apply(0))) { fd =>
+        solve(fd.mkString)
+      }
+    }
+  }
+
+}
