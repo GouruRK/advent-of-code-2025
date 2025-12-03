ed3c5a8c7bf7127eff0e031b58ec0d47575e82a8
GouruRK <gouru.rk.gouru.rk@gmail.com>
2025-12-03 21:44:29 +0100

    add day 3 part 1

---
+++ src/fr/aoc2025/day03/AdventOfCode03P01.scala
@@ -0,0 +1,54 @@
+package fr.aoc2025.day03
+
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.Using
+
+object AdventOfCode03P01 {
+
+  private def solve(input: String): Int = {
+    solve(input.split("\n").map { _.strip }.map { x => x.chars().map { _ - '0' }.toArray.toList }.toList)
+  }
+
+  private def indexOfMax(list : List[Int]) : (Int, Int) = {
+    @tailrec
+    def indexOfMax(l: List[(Int, Int)], maxElement: Int, maxIndex: Int): (Int, Int) = {
+      l match {
+        case Nil => (maxElement, maxIndex)
+        case (a, _)::others if a <= maxElement => indexOfMax(others, maxElement, maxIndex)
+        case (a, i)::others => indexOfMax(others, a, i)
+      }
+    }
+    indexOfMax(list.zipWithIndex, -1, -1)
+  }
+
+  @tailrec
+  private def solve(banks: List[List[Int]], acc: Int = 0): Int = {
+    banks match {
+      case Nil => acc
+      case bank::others =>
+        val (a, i) = indexOfMax(bank)
+        if (i == bank.size - 1) {
+          val subList = bank.slice(0, i)
+          val (b, j) = indexOfMax(subList)
+          solve(others, acc + b*10 + a)
+        } else {
+          val subList = bank.slice(i + 1, bank.size)
+          val (b, j) = indexOfMax(subList)
+          solve(others, acc + a*10 + b)
+        }
+    }
+  }
+
+  private def solveWithFile(filename: String): Long = {
+    Using(Source.fromFile(filename)) { fd =>
+      solve(fd.mkString.stripTrailing)
+    }.get
+  }
+
+  def main(args: Array[String]) : Unit = {
+    println("Example: " + solveWithFile("inputs/day03-example.txt"))
+    println("Problem: " + solveWithFile("inputs/day03.txt"))
+  }
+
+}
